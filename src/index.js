fd3878398123d48eec21e3c07911923a2e07e877
Alexei Lashakov <alexei.lashakov@mail.ru>
2018-09-19 23:40:00 +0300

    Added implementation of function getLoveTrianglesCount to index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    var amount = 0;
+    for (var i = 0; i < preferences.length; i++) {
+        a = preferences[i];
+        b = preferences[a - 1];
+        c = preferences[b - 1];
+        if (a == b && a == c && b == c) continue;
+        if (c - 1 == i) {
+            amount++;
+            preferences[i] = null;
+        }
+    }
+    return amount;
 };
